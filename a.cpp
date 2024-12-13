9782113a4ae41cfec765bf2c50b8705b377c7016
niusha-kh <niushaw.kh@gmail.com>
2024-12-13 15:53:20 +0330

    Add files via upload
    4a

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int w;
+
+int main() {
+	cin >> w;
+	if (w % 2 == 0 && w != 2)	{
+		cout << "yes" << endl;
+	}
+	else {
+		cout << "no" << endl;
+	}
+	return 0;
+}
